4040a693c1b0ec9e6338645c2cc46f218a7b3129
tvcuong212 <133182272+tvcuong212@users.noreply.github.com>
2023-08-03 08:45:47 +0700

    spa

---
+++ 230801/js/app.js
@@ -0,0 +1,35 @@
+var app = angular.module('myApp',['ngRoute']);
+
+app.config(function($routeProvider){
+    $routeProvider
+
+    .when('/', {
+        templateUrl : 'pages/home.html',
+        controller  : 'HomeController'
+    })
+
+    .when('/blog', {
+        templateUrl : 'pages/blog.html',
+        controller  : 'BlogController'
+    })
+
+    .when('/about', {
+        templateUrl : 'pages/about.html',
+        controller  : 'AboutController'
+    })
+
+    .otherwise({redirectTo: '/'});
+
+});
+
+app.controller('HomeController', function($scope){
+    $scope.message = 'Hello froom HomeController';
+});
+
+app.controller('BlogController', function($scope){
+    $scope.message = 'Hello froom BlogController';
+});
+
+app.controller('AboutController', function($scope){
+    $scope.message = 'Hello froom AboutController';
+});
